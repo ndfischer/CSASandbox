6c2488fe353e3614059a56529fa7a2148ce4ec93
s166788 <s166788@scusd.net>
2026-05-26 09:50:46 -0700

    Update Cattle constructor description

--- Cattle.java
+++ Cattle.java
@@ -1,5 +1,12 @@
+// --- FILE: Cattle.java ---
+/**
+ * Manages physical livestock assets tracking operations inside ranch fields.
+ */
 public class Cattle extends Livestock {
-    public Cattle() {
-        super("Cattle", "Graze peacefully in the pasture.", 10, 5, 10);
-    }
+  /**
+   * Instantiates the primary livestock tracking asset variant.
+   */
+  public Cattle() {
+    super("Cattle", "Domesticated beef and dairy livestock herd units.", 10, 5, 10);
+  }
 }
