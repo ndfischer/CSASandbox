1537c289de8081d3405852a7a01168659511d7c2
Nico Fischer <fischer.nico.nf08@gmail.com>
2026-05-26 12:16:49 -0700

    PRE-FINAL version

--- Item.java
+++ Item.java
@@ -1,17 +1,49 @@
+/**
+ * Base class representing an interactable object or entity.
+ *
+ * Provides core properties like name and description that are extended
+ * by specific resource and entity types throughout the game.
+ */
 public class Item {
-    protected String name;
-    protected String description;
+  private String name;
+  private String description;
 
-    public Item(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
+  /**
+   * Constructs a basic Item.
+   *
+   * @param name the name of the item
+   * @param description the description of the item
+   */
+  public Item(String name, String description) {
+    this.name = name;
+    this.description = description;
+  }
 
-    public String getName() {
-        return name;
-    }
+  /**
+   * Retrieves the name of the item.
+   *
+   * @return the item's name
+   */
+  public String getName() {
+    return name;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  /**
+   * Retrieves the description of the item.
+   *
+   * @return the item's description
+   */
+  public String getDescription() {
+    return description;
+  }
+
+  /**
+   * Returns a string representation of the item.
+   *
+   * @return the item's name
+   */
+  @Override
+  public String toString() {
+    return name;
+  }
 }
