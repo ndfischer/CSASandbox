06df0f2583b2ae232c07dce3be1741185eca5cd1
Nico Fischer <fischer.nico.nf08@gmail.com>
2026-05-26 12:17:36 -0700

    PRE-FINAL version

--- Gold.java
+++ Gold.java
@@ -1,5 +1,17 @@
+/**
+ * Represents gold ore found in the American River and Sierra Mine.
+ *
+ * Extends Ore with a predefined price value.
+ */
 public class Gold extends Ore {
-    public Gold() {
-        super("Gold", "Native gold gleams from deeper stone.", 0.50);
-    }
+
+  /**
+   * Constructs a Gold ore item.
+   *
+   * @param name the name of the gold entity
+   * @param description the description of the gold entity
+   */
+  public Gold(String name, String description) {
+    super(name, description, 2.50);
+  }
 }
