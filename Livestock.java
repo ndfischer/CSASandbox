e665066f3cad5073d0130ae154ee3ef3949fc18f
Nico Fischer <fischer.nico.nf08@gmail.com>
2026-05-26 13:35:56 -0700

    FINAL VERSION

--- Livestock.java
+++ Livestock.java
@@ -1,7 +1,9 @@
+// --- FILE: Livestock.java ---
 /**
- * Represents livestock raised by the player.
+ * Represents livestock animals in the game.
  *
- * Extends Item to include production cycles and goods yield.
+ * Demonstrates inheritance by extending Item and adding fields for production
+ * tracking. Serves as a base class for specific animals like Cattle.
  */
 public class Livestock extends Item {
   private int productionInterval;
@@ -9,45 +11,18 @@ public class Livestock extends Item {
   private int goodsSellPrice;
 
   /**
-   * Constructs a Livestock item.
+   * Constructs a new Livestock item.
    *
    * @param name the name of the livestock
    * @param description the description of the livestock
    * @param productionInterval the number of turns required to produce goods
-   * @param productionAmount the amount of goods produced per cycle
-   * @param goodsSellPrice the price the goods sell for
+   * @param productionAmount the amount of goods produced per interval
+   * @param goodsSellPrice the price the produced goods sell for
    */
   public Livestock(String name, String description, int productionInterval, int productionAmount, int goodsSellPrice) {
     super(name, description);
     this.productionInterval = productionInterval;
     this.productionAmount = productionAmount;
     this.goodsSellPrice = goodsSellPrice;
-  }
-
-  /**
-   * Retrieves the turn interval required for production.
-   *
-   * @return the interval as an integer
-   */
-  public int getProductionInterval() {
-    return productionInterval;
-  }
-
-  /**
-   * Retrieves the amount of goods produced.
-   *
-   * @return the amount of goods
-   */
-  public int getProductionAmount() {
-    return productionAmount;
-  }
-
-  /**
-   * Retrieves the sell price of the goods.
-   *
-   * @return the price of goods
-   */
-  public int getGoodsSellPrice() {
-    return goodsSellPrice;
   }
 }
